9c7686cdbff37a52fa7c7ea131fcf064f477d60b
MOCH84BDG <91376150+budijoi@users.noreply.github.com>
2023-01-04 21:46:01 +0700

    Update yacd.sh
    Disable shadowsock

--- yacd.sh
+++ yacd.sh
@@ -6,14 +6,14 @@ pkg install git
 pkg install micro
 pkg install unzip
 cd .config/clash
-git clone https://github.com/Alamyazid/yacd-gh-pages
-cd yacd-gh-pages
+git clone https://github.com/budijoi/yacd
+cd yacd
 cp -r -f config.yaml $HOME/.config/clash
 cp -r -f Country.mmdb $HOME/.config/clash
 cp -r -f vmess.yaml $HOME/.config/clash
 cp -r -f trojan.yaml $HOME/.config/clash
 cp -r -f trojan-go.yaml $HOME/.config/clash
-cp -r -f shadowsocks.yaml $HOME/.config/clash
+#cp -r -f shadowsocks.yaml $HOME/.config/clash
 cp -r -f yacd-gh-pages1.zip $HOME/.config/clash
 cp -r -f yacd-gh-pages2.zip $HOME/.config/clash
 cp -r -f yacd-gh-pages3.zip $HOME/.config/clash
